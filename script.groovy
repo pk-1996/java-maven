4f2b10923af50283836f051755de2b1eff3fe535
prasadk10 <prasadkumbhar817@gmail.com>
2022-12-20 22:40:22 +0530

    stupid brackets

--- script.groovy
+++ script.groovy
@@ -4,10 +4,11 @@ def buildJar() {
 }
 
 def buildImage() {
-    withCredentials([usernameColonPassword(credentialsId: 'dockerhub-creds', passwordVariable: 'PASS', usernameVariable: 'USER')])
+    withCredentials([usernameColonPassword(credentialsId: 'dockerhub-creds', passwordVariable: 'PASS', usernameVariable: 'USER')]){
         sh 'docker build -t prasadk10/my-repo:jma-2.0 .'
         sh "echo $PASS | docker login -u $USER --password-stdin"
         sh 'docker push prasadk10/my-repo:jma-2.0'
+    }
 }
 
 def deploy() {
