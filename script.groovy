f401f1ed8e0433d95bdce15273a2ab77b4312ce6
prasadk10 <112400948+prasadk10@users.noreply.github.com>
2023-05-07 22:54:52 +0530

    Update script.groovy

--- script.groovy
+++ script.groovy
@@ -4,7 +4,8 @@ def buildJar() {
 }
 
 def buildImage() {
-    withCredentials([usernameColonPassword(credentialsId: 'dockerhub-creds', passwordVariable: 'PASS', usernameVariable: 'USER')]){
+    echo "building docker image"
+    withCredentials([usernamePassword(credentialsId: 'dockerhub-creds', passwordVariable: 'PASS', usernameVariable: 'USER')]) {
         sh 'docker build -t prasadk10/my-repo:jma-2.0 .'
         sh "echo $PASS | docker login -u $USER --password-stdin"
         sh 'docker push prasadk10/my-repo:jma-2.0'
